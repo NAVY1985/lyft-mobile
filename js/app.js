d044bd781ab971c9ebdac165300c4cbbf4869424
navy1985 <jezreelsanz@gmail.com>
2018-06-22 12:38:09 -0500

    se hacen ultimas modificaciones a los estilos

--- js/app.js
+++ js/app.js
@@ -1,27 +1,11 @@
-
-
-
-
-
-
-
-
-
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
 
-  $(document).ready(function() {
-    setTimeout(function() {
-        $(".container1").fadeOut(1500);
-    },1000);
-});
-
-$(document).ready(function () {
-    $("#phone-number").keypress(function () {
+  $("#phone-number").keypress(function () {
         var valuePhone = $("#phone-number").val().length;
         console.log(valuePhone);
-        if (valuePhone >= 10 ) {
+        if (valuePhone >= 10) {
             $("#next-button").removeAttr("disabled");
             $("#phone-number").attr("disabled", "disabled");
         }
@@ -29,12 +13,16 @@ $(document).ready(function () {
     });
     var $randomNumber = 0;
     $randomNumber = parseInt(Math.random()*1000) ;
-    console.log("LAB-"+ $randomNumber);
    
    $("#next-button").click(function(){
     swal("Tu codigo de verificación es : LAB- " +  $randomNumber);
    });
    
- });
+   $(document).ready(function() {
+    setTimeout(function() {
+        $(".container1").fadeOut(1500);
+    },1000);
+});
+ 
 
  
